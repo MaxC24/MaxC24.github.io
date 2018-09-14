d1cd1dcae702289c1e1fd7b1c1235591d4038cc5
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2018-09-14 10:40:08 +0200

    created a pdf and moved resume in it

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ $(document).ready(function(){
 
 		//RESUME BUTTON
 		// create the resume dom Element and attach it to a display none lightbox that will fade/in at resume btn onclick
-		var $resume = $('<img class="resume" src="./img/resume.jpg"/><div id="download"><a  href="img/Massimo-Crapanzano-Resume.pdf">Download Pdf</a></div>');
+		var $resume = $('<img class="resume" src="./img/resume.jpg"/><div id="download"><a  href="pdf/Massimo-Crapanzano-Resume.pdf">Download Pdf</a></div>');
 		$resume = addToLightBox($resume);
 		addOnClickEvent($resume);
 
